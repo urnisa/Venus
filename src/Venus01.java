a3b1395b4bee21da40557e8de37d266d378b136d
Bekbolot E <bekoxna@gmail.com>
2024-05-20 21:08:18 +0600

    User beckbolot , Venus01.java file modified

--- src/Venus01.java
+++ src/Venus01.java
@@ -1,6 +1,7 @@
 public class Venus01 {
     public static void main(String[] args) {
         System.out.println("Venus");
+        System.out.println("User beckbolot,modifying Venus01.java ");
     }
 }
 
