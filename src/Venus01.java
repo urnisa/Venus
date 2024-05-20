120de021fb375495791a4d54bb3e2d4aceb61c64
Bekbolot E <bekoxna@gmail.com>
2024-05-20 21:37:41 +0600

    user beck, modifing Venus01 2 nd time

--- src/Venus01.java
+++ src/Venus01.java
@@ -1,7 +1,8 @@
 public class Venus01 {
     public static void main(String[] args) {
         System.out.println("Venus");
-        System.out.println("User beckbolot,modifying Venus01.java ");
+        System.out.println("User beckbolot,modifying Venus01.java 2nd time");
+
     }
 }
 
